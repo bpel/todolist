5bf1254d07e88d86204a6b9710782004e270f252
benjamin.pelletier <pelletierbenjamin90@gmail.com>
2019-11-30 16:01:44 +0100

    Update TaskVoter to use anonymous user #46

--- src/AppBundle/Security/TaskVoter.php
+++ src/AppBundle/Security/TaskVoter.php
@@ -44,7 +44,7 @@ class TaskVoter extends Voter implements VoterInterface
 
         $task = $subject;
 
-        if(null == $task->getAuthor())
+        if('anonymous' == $task->getAuthor()->getUsername())
         {
             return $this->decisionManager->decide($token, ['ROLE_ADMIN']);
         }
