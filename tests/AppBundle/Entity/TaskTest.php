1335814d6ac62b1ba99cb5632f9ed7dc6d3aa820
benjamin.pelletier <pelletierbenjamin90@gmail.com>
2019-12-23 18:44:25 +0100

    Update unit tests of entity Task, User & rename tests/Util directory to tests/Entity

--- tests/AppBundle/Entity/TaskTest.php
+++ tests/AppBundle/Entity/TaskTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\AppBundle\Util;
+namespace Tests\AppBundle\Entity;
 
 use AppBundle\Entity\Task;
 use AppBundle\Entity\User;
@@ -69,5 +69,14 @@ class TaskTest extends TestCase
     {
         $this->task->setAuthor($this->author);
         $this->assertEquals($this->author, $this->task->getAuthor());
+    }
+
+    /**
+     * @test
+     * Test author get id
+     */
+    public function testAuthorGetId()
+    {
+        $this->assertEquals(null, $this->task->getId());
     }
 }
--- tests/AppBundle/Entity/UserTest.php
+++ tests/AppBundle/Entity/UserTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\AppBundle\Util;
+namespace Tests\AppBundle\Entity;
 
 use AppBundle\Entity\Task;
 use AppBundle\Entity\User;
@@ -66,5 +66,25 @@ class UserTest extends TestCase
     {
         $this->user->setRoles(['ROLE_USER']);
         $this->assertEquals(array('0' => 'ROLE_USER'), $this->user->getRoles());
+    }
+
+    /**
+     * @test
+     * Test erase credentials
+     */
+    public function testEraseCredentials()
+    {
+        $user = $this->user;
+        $this->user->eraseCredentials();
+        $this->assertEquals($user, $this->user);
+    }
+
+    /**
+     * @test
+     * Test user get id
+     */
+    public function testUserGetId()
+    {
+        $this->assertEquals(null, $this->user->getId());
     }
 }
